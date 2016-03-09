78698ac6753e078f160818034c78dcdae7b900ff
Hamza Alalach <hamzaalalach@gmail.com>
2016-03-09 21:18:44 +0000

    Add menu

--- base.js
+++ base.js
@@ -77,19 +77,77 @@ function store(str) {
 }
 
 
-/*  Delete Elements  */
+/*  Delete/Edit Elements  */
 (function() {
 	var lis = document.querySelectorAll('#todo li');
 	var lisL = lis.length;
+	var divE = false;
 	for (var i = 0; i < lisL; i++) {
-		lis[i].addEventListener('click', function(e) {
+		lis[i].addEventListener('dblclick', function(e) {
 			var eId = e.target.id;
-			gItems = $.grep(gItems, function(value) {
-				return value != gItems[eId];
-			});
-	 		var items = gItems;
-	 		localStorage["items"] = JSON.stringify(items);
-	 		window.location.reload();
+			var left = e.clientX;
+			var top = e.clientY;
+			var div;
+			if (divE) {
+				hideMenu();
+			} else {
+				div = document.createElement('div');
+				divE = true;
+				document.getElementsByTagName('body')[0].appendChild(div);
+				openMenu();
+			}
+			function openMenu() {
+				div.className = 'menu';
+				div.style.top = top + 'px';
+				div.style.left = left + 'px';
+				var ul = document.createElement('ul'),
+					li1 = document.createElement('li'),
+					li1Txt = document.createTextNode('Edit');
+				li1.appendChild(li1Txt);
+				var li2 = document.createElement('li'),
+					li2Txt = document.createTextNode('Remove');
+				li2.appendChild(li2Txt);
+				ul.appendChild(li1);
+				ul.appendChild(li2);
+				div.appendChild(ul);
+
+				li2.addEventListener('click', function() {
+					hideMenu();
+					remove();
+				}, false)
+				li1.addEventListener('click', function() {
+					hideMenu();
+					edit();
+				}, false)
+			}
+			function hideMenu() {
+				var div = document.querySelector('.menu')
+				div.parentNode.removeChild(div);
+				divE = false;
+			}
+			function remove() {
+				gItems = $.grep(gItems, function(value) {
+					return value != gItems[eId];
+				});
+		 		var items = gItems;
+		 		localStorage["items"] = JSON.stringify(items);
+		 		window.location.reload();
+		 	}
+		 	function edit() {
+		 		var modElem = prompt('Editing...', e.target.innerHTML);
+		 		if (modElem != e.target.innerHTML && modElem.length >= 3) {
+		 			var gItemsL = gItems.length;
+					for (var i = 0; i < gItemsL; i++) {
+						gItems[eId] = modElem;
+					}
+			 		var items = gItems;
+			 		localStorage["items"] = JSON.stringify(items);
+			 		window.location.reload();
+		 		} else {
+		 			return false;
+		 		}
+				
+		 	}
 		}, false)
 	}
 })();
