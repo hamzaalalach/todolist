1622fb264d19e95fa3498cdde0a00897475e2a66
hamzaalalach <hamzaalalach@gmail.com>
2016-09-24 23:07:44 +0100

    Use EJS and create home router

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var express = require('express'),
 	helmet = require('helmet'),
 	compression = require('compression'),
 	bodyParser = require('body-parser'),
+	home = require('./routes/home'),
 	add = require('./routes/add');
+app.set('view engine', 'ejs');
 app.use(compression());
 app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,6 +16,7 @@ app.use(favicon(path.join(__dirname, '/public/images/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(home);
 app.use(add);
 app.use(function(req, res, next) {
 	res.status(404).send('Not found!');
