391f2dda09b50a6f7d9719b678827f0e3cfe52e6
Hamza Alalach <hamzaalalach@gmail.com>
2017-02-21 14:14:46 +0000

    Add remove all route

--- routes/remove.js
+++ routes/remove.js
@@ -1,7 +1,10 @@
 var express = require('express'),
 	router = express.Router(),
 	actions = require('../bin/actions');
-router.get('/remove/:id', function(req, res, next) {
+router.get('/remove/:id', function(req, res) {
 	actions.removeOne(req.params.id);
+});
+router.get('/remove/', function(req, res) {
+	actions.removeAll();
 });
 module.exports = router;
