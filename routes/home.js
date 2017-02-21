7389c2cdace40c24df9c2957d592ed7e000b9240
Hamza Alalach <hamzaalalach@gmail.com>
2017-02-21 11:15:37 +0000

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,6 @@
 var express = require('express'),
 	router = express.Router(),
-	actions = require('./bin/actions');
+	actions = require('../bin/actions');
 router.get('/', function(req, res, next) {
 	actions.findAll(function(data) {
 		res.render('index', {data});
